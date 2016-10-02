e52929733b31e693e1ae1bfc1d786c8fda92f270
Arsalan Gundroo <arsalang@tavisca.com>
2016-10-02 21:17:58 +0530

    Linear regression in one variable completed

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,21 +16,19 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    gap=X*theta - y;
+    delta=zeros(size(X,2),1);
+    for i = 1:m
+      delta=delta+ gap(i).*X'(:,i);
+    end
+    delta=delta/m;
 
-
-
-
-
-
-
-
-
-
-
+    theta=theta-(alpha*delta);
+    fprintf('%f %f %f \n', theta(1), theta(2), theta(3));
     % ============================================================
 
     % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
+    J_history(iter) = computeCost(X, y, theta);
 
 end
 
